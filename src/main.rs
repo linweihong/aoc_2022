0525a9aabad9f5f1af71040a1decd34da490c04a
Weihong <weihong@duck.com>
2022-12-18 01:14:07 +0800

    Implement AOC2022 day 2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ const INPUT_1_1: &str = "./inputs/1_1.txt";
 const INPUT_2_1: &str = "./inputs/2_1.txt";
 
 fn main() {
-    // aoc_1();
+    aoc_1();
     aoc_2();
 }
 
@@ -30,35 +30,92 @@ fn aoc_1() {
         }
     }
     elves.sort();
-    println!("aoc_1_1 - max_cal: {}", elves[elves.len() - 1]);
+    println!("1_1: {}", elves[elves.len() - 1]);
     let mut top_three_elves: i32 = 0;
     for i in elves.len() - 3..elves.len() {
         top_three_elves += elves[i];
     }
-    println!("aoc_1_2 - top three elves: {}", top_three_elves);
+    println!("1_2: {}", top_three_elves);
 }
 
 fn aoc_2() {
     let contents: String = fs::read_to_string(INPUT_2_1).expect("File access error");
 
+    let mut scores_1 = 0;
+    let mut scores_2 = 0;
     for element in contents.split("\n") {
         // println!("{element}");
-        scoring(&element[..]);
+        let (score_1, score_2) = scoring(&element[..]);
+        scores_1 += score_1;
+        scores_2 += score_2;
     }
+    println!("2_1: {scores_1}");
+    println!("2_2: {scores_2}");
 
-    fn scoring(round: &str) -> i32 {
-        let mut score = 0;
-        for c in round.chars(){
-            println!("{c}")
-            // TODO: Implement scoring
+    fn scoring(round: &str) -> (i32, i32) {
+        let mut score_1 = 0;
+        let mut score_2 = 0;
+        let mut plays = Vec::<char>::new();
+        for c in round.chars() {
+            if c != ' ' {
+                plays.push(c)
+            }
+            // println!("{plays:#?}")
         }
-        // println!("{}", round[0]);
-        // let opponent = round[0];
-        // let player = round[2];
-        // println!("opponent: {opponent}, player: {player}");
-        // match round {
-        //     (round[0] == "A" && round[2] == "X") => 4,
-        // }
-        5
+
+        if plays[1] == 'X' {
+            score_1 += 1;
+        } else if plays[1] == 'Y' {
+            score_1 += 2;
+        } else if plays[1] == 'Z' {
+            score_1 += 3;
+        }
+
+        if plays[0] == 'A' {
+            if plays[1] == 'X' {
+                score_1 += 3;
+            } else if plays[1] == 'Y' {
+                score_1 += 6;
+            }
+        } else if plays[0] == 'B' {
+            if plays[1] == 'Y' {
+                score_1 += 3;
+            } else if plays[1] == 'Z' {
+                score_1 += 6;
+            }
+        } else if plays[0] == 'C' {
+            if plays[1] == 'X' {
+                score_1 += 6;
+            } else if plays[1] == 'Z' {
+                score_1 += 3;
+            }
+        }
+
+        if plays[0] == 'A' {
+            if plays[1] == 'X' {
+                score_2 += 3;
+            } else if plays[1] == 'Y' {
+                score_2 += 4;
+            } else if plays[1] == 'Z' {
+                score_2 += 8;
+            }
+        } else if plays[0] == 'B' {
+            if plays[1] == 'X' {
+                score_2 += 1;
+            } else if plays[1] == 'Y' {
+                score_2 += 5;
+            } else if plays[1] == 'Z' {
+                score_2 += 9;
+            }
+        } else if plays[0] == 'C' {
+            if plays[1] == 'X' {
+                score_2 += 2;
+            } else if plays[1] == 'Y' {
+                score_2 += 6;
+            } else if plays[1] == 'Z' {
+                score_2 += 7;
+            }
+        }
+        return (score_1, score_2);
     }
 }
