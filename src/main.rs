064b72cf6cb740333fe97af733acdb4e64b3d67a
Weihong <weihong@duck.com>
2022-12-17 23:23:17 +0800

    Partial implementation of AOC2022 day 2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::fs;
 
 const INPUT_1_1: &str = "./inputs/1_1.txt";
+const INPUT_2_1: &str = "./inputs/2_1.txt";
 
 fn main() {
-    aoc_1();
+    // aoc_1();
+    aoc_2();
 }
 
 fn aoc_1() {
-    let contents = fs::read_to_string(INPUT_1_1).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(INPUT_1_1).expect("File access error");
 
     let mut elves = Vec::<i32>::new();
     let mut cal: i32 = 0;
@@ -34,4 +36,29 @@ fn aoc_1() {
         top_three_elves += elves[i];
     }
     println!("aoc_1_2 - top three elves: {}", top_three_elves);
+}
+
+fn aoc_2() {
+    let contents: String = fs::read_to_string(INPUT_2_1).expect("File access error");
+
+    for element in contents.split("\n") {
+        // println!("{element}");
+        scoring(&element[..]);
+    }
+
+    fn scoring(round: &str) -> i32 {
+        let mut score = 0;
+        for c in round.chars(){
+            println!("{c}")
+            // TODO: Implement scoring
+        }
+        // println!("{}", round[0]);
+        // let opponent = round[0];
+        // let player = round[2];
+        // println!("opponent: {opponent}, player: {player}");
+        // match round {
+        //     (round[0] == "A" && round[2] == "X") => 4,
+        // }
+        5
+    }
 }
