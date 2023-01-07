69fd2d54cd020f0ff744d2f89b5e10721c65432a
Weihong <39170719+linweihong@users.noreply.github.com>
2023-01-07 23:33:21 +0700

    implement divmod

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,7 @@ pub fn get_input(src: &str) -> String {
     let inputs = fs::read_to_string(src).expect("File access error");
     return inputs;
 }
+
+pub fn divmod(a: i32, b: i32) -> (i32, i32) {
+    return (a % b, a / b)
+}
